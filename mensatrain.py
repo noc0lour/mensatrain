e8baaa5a9498a93e34fc8dc4ca856c8a47cd781b
Dominik Rimpf <dev@d-rimpf.de>
2020-02-02 13:57:00 +0100

    New Telegram API Compatibility
    Fixed a lot of stuff:
    - new api-version of python-telgram-bot
    - optimizied imports
    - reformated code
    - fixed sql-querys (is instead of == didn't work for me)
    - format-strings are now f-strings
    - added Unique-Constraint to schedule-table

--- mensatrain.py
+++ mensatrain.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
-import telegram
-from telegram.error import TelegramError
-from telegram.ext import Updater, CommandHandler, RegexHandler
+import argparse
+import datetime
+import inspect
+import json
+import logging
+import random
 import urllib.request
 
+import dateutil.parser
 import sqlalchemy
+import sqlalchemy.exc
+import tabulate
+import telegram
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.pool import NullPool
-
-import dateutil.parser
-import datetime
-import tabulate
-import argparse
-
-import json
-import inspect
-import logging
-import random
+from telegram import Update
+from telegram.error import TelegramError
+from telegram.ext import Updater, CommandHandler, CallbackContext, Filters, MessageHandler
 
 Base = declarative_base()
 
@@ -42,6 +42,7 @@ class ScheduleMap(Base):
     owner = sqlalchemy.Column(sqlalchemy.Integer,
                               sqlalchemy.ForeignKey("users.id"))
     valid = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
+    __table_args__ = (sqlalchemy.UniqueConstraint(date, station),)
 
 
 class TicketMap(Base):
@@ -68,40 +69,34 @@ class UserMap(Base):
 
 
 def access_restricted(func):
-    def access_wrapper(self, bot, update, *args, **kwargs):
+    def access_wrapper(self, update: Update, context: CallbackContext):
         try:
             arguments = parse_args()
             if arguments.restricted_group:
-                user = bot.get_chat_member(
+                user = context.bot.get_chat_member(
                     arguments.restricted_group,
                     update.effective_user.id
                 )
                 if user.status in ("member", "admin", "creator"):
-                    return func(self, bot, update, *args, **kwargs)
+                    return func(self, update, context)
                 else:
-                    update.message.reply_text(
-                            "You have not the correct permissions to use this \
-                            functionality."
-                    )
+                    update.message.reply_text("You have not the correct permissions to use this functionality.")
                     return
             else:
-                return func(self, bot, update, *args, **kwargs)
+                return func(self, update, context)
         except TelegramError:
-            update.message.reply_text(
-                "This bot is only for some users 😉."
-            )
+            update.message.reply_text("This bot is only for some users 😉.")
             return
     return access_wrapper
 
 
 class MensaTrainBot(object):
     def __init__(self, db_file):
-        engine = sqlalchemy.create_engine(
-            'sqlite:///{}'.format(db_file), poolclass=NullPool)
+        engine = sqlalchemy.create_engine(f'sqlite:///{db_file}', poolclass=NullPool)
         Base.metadata.create_all(engine, checkfirst=True)
         self.session = sessionmaker(bind=engine)
 
-    def help(self, bot, update):
+    def help(self, update: Update, context: CallbackContext):
         """Send a message when the command /help is issued."""
         update.message.reply_text(
             inspect.cleandoc(
@@ -113,26 +108,21 @@ class MensaTrainBot(object):
 
     def parse_args(self, args):
         if args is None or len(args) < 2:
-            return (
-                None,
-                "Not enough arguments. Please provide time and station name.")
+            return None, "Not enough arguments. Please provide time and station name."
         try:
             date = dateutil.parser.parse(str(args[0]))
         except ValueError or OverflowError:
-            return (None, "Invalid date format")
+            return None, "Invalid date format"
 
         if not datetime.date.today() == date.date():
-            return (None,
-                    "Schedule planning is only supported for the same day")
+            return None, "Schedule planning is only supported for the same day"
 
-        if (date.hour > 14) or (date.hour == 14 and date.minute > 30) or (
-                date.hour < 11):
-            return (None, "Schedule planning only possible from 11:00 until \
-            14:30")
+        if (date.hour > 15) or (date.hour == 15 and date.minute > 30) or (date.hour < 11):
+            return None, "Schedule planning only possible from 11:00 until 15:30"
 
-        return (date, args[1])
+        return date, args[1]
 
-    def get_user(self, update):
+    def get_user(self, update: Update):
         user_id = update.effective_user.id
         session = self.session()
         user = session.query(UserMap).filter_by(tid=user_id)
@@ -146,7 +136,7 @@ class MensaTrainBot(object):
             user = session.query(UserMap).filter_by(tid=user_id)
         return user.one()
 
-    def get_user_journey(self, update):
+    def get_user_journey(self, update: Update):
         session = self.session()
         user_id = update.effective_user.id
         # Check if the user already has a ticket for today
@@ -154,14 +144,14 @@ class MensaTrainBot(object):
             tid=user_id).join(TicketMap).join(ScheduleMap).filter(
                 ScheduleMap.date > datetime.date.today(), ScheduleMap.date <
                 datetime.date.today() + datetime.timedelta(1),
-                TicketMap.valid is True)
+                TicketMap.valid == True)
         return user_journeys.one_or_none()
 
-    def get_user_ticket(self, update):
+    def get_user_ticket(self, update: Update):
         session = self.session()
         user = self.get_user(update)
         user_ticket = session.query(TicketMap).filter(
-            TicketMap.valid is True,
+            TicketMap.valid == True,
             TicketMap.uid == user.id).join(ScheduleMap).filter(
                 ScheduleMap.date > datetime.date.today(),
                 ScheduleMap.date <
@@ -176,16 +166,15 @@ class MensaTrainBot(object):
     def get_trains_today(self):
         session = self.session()
         schedule_information = []
-        journeys = session.query(ScheduleMap).filter(
-            ScheduleMap.date > datetime.date.today(),
-            ScheduleMap.date < datetime.date.today() + datetime.timedelta(1),
-            ScheduleMap.valid is True).order_by(ScheduleMap.date)
+        journeys = session.query(ScheduleMap).filter(ScheduleMap.date > datetime.date.today(),
+                                                     ScheduleMap.date < datetime.date.today() + datetime.timedelta(1),
+                                                     ScheduleMap.valid == True).order_by(ScheduleMap.date)
         for j in journeys:
             participants = session.query(TicketMap).filter(
                 TicketMap.sid == j.id,
                 TicketMap.valid == True).join(UserMap).all()  # noqa
             schedule_information.append([
-                j.id, ":".join((str(j.date.hour), str(j.date.minute))),
+                j.id, f"{j.date.hour:02d}:{j.date.minute:02d}",
                 j.station, ", ".join([
                     "[" + p.user.fullname + "](tg://user?id=" + p.user.tid +
                     ")" for p in participants
@@ -194,7 +183,7 @@ class MensaTrainBot(object):
         return schedule_information
 
     @access_restricted
-    def schedule(self, bot, update):
+    def schedule(self, update: Update, context: CallbackContext):
         """
         """
         schedule_information = self.get_trains_today()
@@ -205,11 +194,12 @@ class MensaTrainBot(object):
         schedule_information_text += tabulate.tabulate(
             schedule_information,
             schedule_information_headers,
-            tablefmt="plain")
-        update.message.reply_markdown(schedule_information_text)
+            tablefmt='plain'
+        )
+        update.message.reply_markdown(f"{schedule_information_text}")
 
     @access_restricted
-    def ticket(self, bot, update, **kwargs):
+    def ticket(self, update: Update, context: CallbackContext):
         """
         """
         session = self.session()
@@ -218,32 +208,20 @@ class MensaTrainBot(object):
         # user_journey = self.get_user_journey(update)
         user_ticket = self.get_user_ticket(update)
         if user_ticket is not None:
-            update.message.reply_text(
-                "Error processing your request: Already registered for a train \
-                today."
-            )
+            update.message.reply_text("Error processing your request: Already registered for a train today.")
             return
 
-        args = self.parse_args(kwargs.get('args', None))
+        args = self.parse_args(context.args)
         if args[0] is None:
             schedule = self.get_trains_today()
             custom_keyboard = [["/ticket " + t[1] + " " + t[2]]
                                for t in schedule]
-            reply_markup = telegram.ReplyKeyboardMarkup(
-                custom_keyboard, one_time_keyboard=True, selective=True)
-            update.message.reply_text(
-                "Select an available train.", reply_markup=reply_markup)
+            reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard, one_time_keyboard=True, selective=True)
+            update.message.reply_text("Select an available train.", reply_markup=reply_markup)
             return
-        journeys = session.query(ScheduleMap).filter_by(
-            date=args[0], station=args[1])
+        journeys = session.query(ScheduleMap).filter_by(date=args[0], station=args[1])
         if journeys.count() == 0:
-            update.message.reply_text(
-                "Error processing your request: No valid journey found.")
-            return
-
-        if journeys.count() > 1:
-            update.message.reply_text(
-                "Error processing your request: Duplicate journeys found.")
+            update.message.reply_text("Error processing your request: No valid journey found.")
             return
 
         user = self.get_user(update)
@@ -251,13 +229,11 @@ class MensaTrainBot(object):
         session.add(TicketMap(sid=journey.id, uid=user.id))
         session.commit()
         update.message.reply_text(
-            "You successfully bought your ticket for the train departing from \
-            {} at {}".
-            format(journey.station, ":".join((str(journey.date.hour), str(
-                journey.date.minute)))))
+            f"You successfully bought your ticket for the train departing from \
+            {journey.station} at {journey.date.hour:02d}:{journey.date.minute:02d}")
 
     @access_restricted
-    def revoke(self, bot, update):
+    def revoke(self, update: Update, context: CallbackContext):
         session = self.session()
         user_ticket = self.get_user_ticket(update)
         if user_ticket is None:
@@ -267,66 +243,67 @@ class MensaTrainBot(object):
         ticket = session.query(TicketMap).filter_by(id=user_ticket.id).one()
         ticket.valid = False
         update.message.reply_text(
-            "Successfully revoked your Ticket for the journey at {} from {}.".
-            format(":".join((str(ticket.journey.date.hour), str(
-                ticket.journey.date.minute))), ticket.journey.station))
+            f"Successfully revoked your Ticket for the journey at {ticket.journey.date.hour:02d}:{ticket.journey.date.minute:02d} from {ticket.journey.station}."
+        )
         session.commit()
 
         return
 
     @access_restricted
-    def add_departure(self, bot, update, **kwargs):
+    def add_departure(self, update: Update, context: CallbackContext):
         """
         """
-        args = self.parse_args(kwargs.get('args', None))
+        args = self.parse_args(context.args)
         if args[0] is None:
-            update.message.reply_text(
-                "Error processing your request: {}".format(args[1]))
+            update.message.reply_text(f"Error processing your request: {args[1]}")
             return
         user = self.get_user(update)
         session = self.session()
-        session.add(ScheduleMap(date=args[0], station=args[1], owner=user.id))
-        session.commit()
-        self.ticket(bot, update, args=args)
+        try:
+            session.add(ScheduleMap(date=args[0], station=args[1], owner=user.id))
+            session.commit()
+            self.ticket(update, context)
+        except sqlalchemy.exc.IntegrityError:
+            update.message.reply_text("Error processing your request: Duplicate journeys found.")
 
-    def error(self, bot, update, error):
+    def error(self, update: Update, context: CallbackContext):
         """Log Errors caused by Updates."""
-        logger.warning('Update "%s" caused error "%s"', update, error)
+        logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-    def wat(self, bot, update):
+    def wat(self, update: Update, context: CallbackContext):
         update.message.reply_document("https://media.giphy.com/media/3WmWdBzqveXaE/giphy.gif", quote=False) # noqa
 
-    def benotet(self, bot, update):
+    def benotet(self, update: Update, context: CallbackContext):
         args = parse_args()
         with urllib.request.urlopen(
-                "http://api.giphy.com/v1/gifs/search?q=exam+grades&api_key={}".format(args.giphy) # noqa
+                f"http://api.giphy.com/v1/gifs/search?q=exam+grades&api_key={args.giphy}" # noqa
                 ) as url:
             data = json.loads(url.read().decode())
             image_count = data['pagination']['total_count']
         random_image_id = random.randint(0, min(image_count, 100))
         with urllib.request.urlopen(
-                "http://api.giphy.com/v1/gifs/search?q=exam+grades&api_key={}&offset={}".format(args.giphy, random_image_id) # noqa
+                f"http://api.giphy.com/v1/gifs/search?q=exam+grades&api_key={args.giphy}&offset={random_image_id}" # noqa
                 ) as url:
             data = json.loads(url.read().decode())
         image_id = data['data'][0]['id']
-        update.message.reply_document("https://media.giphy.com/media/{}/giphy.gif".format(image_id), quote=False) # noqa
+        update.message.reply_document(f"https://media.giphy.com/media/{image_id}/giphy.gif", quote=False) # noqa
 
-    def giphy(self, bot, update):
+    def giphy(self, update: Update, context: CallbackContext):
         args = parse_args()
         search_text = update.message.text.lower().lstrip('/')
         with urllib.request.urlopen(
-                "http://api.giphy.com/v1/gifs/search?q={}&api_key={}".format(search_text, args.giphy) # noqa
+                f"http://api.giphy.com/v1/gifs/search?q={search_text}&api_key={args.giphy}" # noqa
                 ) as url:
             data = json.loads(url.read().decode())
             image_count = data['pagination']['total_count']
         random_image_id = random.randint(0, max(min(image_count-1, 100), 0))
         with urllib.request.urlopen(
-                "http://api.giphy.com/v1/gifs/search?q={}&api_key={}&offset={}".format(search_text, args.giphy, random_image_id) # noqa
+                f"http://api.giphy.com/v1/gifs/search?q={search_text}&api_key={args.giphy}&offset={random_image_id}" # noqa
                 ) as url:
             data = json.loads(url.read().decode())
 
         image_id = data['data'][0]['id']
-        update.message.reply_document("https://media.giphy.com/media/{}/giphy.gif".format(image_id), quote=False) # noqa
+        update.message.reply_document(f"https://media.giphy.com/media/{image_id}/giphy.gif", quote=False) # noqa
 
 
 def parse_args():
@@ -344,7 +321,7 @@ def main():
     """Start the bot."""
     # Create the EventHandler and pass it your bot's token.
     args = parse_args()
-    updater = Updater(args.token)
+    updater = Updater(args.token, use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
@@ -352,22 +329,15 @@ def main():
     mybot = MensaTrainBot("fahrplan.db")
 
     # on different commands - answer in Telegram
-    dp.add_handler(
-        CommandHandler(
-            "add_departure",
-            mybot.add_departure,
-            pass_args=True,
-            pass_user_data=True))
-    dp.add_handler(
-        CommandHandler(
-            "ticket", mybot.ticket, pass_args=True, pass_user_data=True))
+    dp.add_handler(CommandHandler("add_departure",  mybot.add_departure))
+    dp.add_handler(CommandHandler("ticket", mybot.ticket))
     dp.add_handler(CommandHandler("schedule", mybot.schedule))
     dp.add_handler(CommandHandler("revoke", mybot.revoke))
     dp.add_handler(CommandHandler("help", mybot.help))
     dp.add_handler(CommandHandler("start", mybot.help))
     dp.add_handler(CommandHandler("wat", mybot.wat))
     dp.add_handler(CommandHandler("benotet", mybot.benotet))
-    dp.add_handler(RegexHandler(".*", mybot.giphy))
+    dp.add_handler(MessageHandler((Filters.regex('(.*)') | Filters.command), mybot.giphy))
 
     # on noncommand i.e message - echo the message on Telegram
     # dp.add_handler(MessageHandler(Filters.text, mybot.echo))
